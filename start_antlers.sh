80313f01261308f18b21357f09588c7d5dd2c22a
brimdor <chrisnelsonx@gmail.com>
2024-10-05 14:33:12 -0500

    fix env vars

--- start_antlers.sh
+++ start_antlers.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /home/antlers/project_antlers
+export PATH_TO_SCRIPTS="/home/antlers/project_antlers"
+export MOTOR_SCRIPT_PATH="$PATH_TO_SCRIPTS/control_motor.py"
+export START_SCRIPT_PATH="$PATH_TO_SCRIPTS/start_antlers.sh"
+export RC_LOCAL="/etc/rc.local"
 python3 app.py
